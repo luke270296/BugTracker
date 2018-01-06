e6b28a0fccf2fd77f8aae36bd664f8051236af3c
luke270296 <34241034+luke270296@users.noreply.github.com>
2018-01-06 22:14:14 +0000

    Login functionality added
    Users are required to login before accessing the system

---
+++ Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bug_Tracking_Application
+{
+    public partial class Login : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Luke\Documents\BugData.mdf;Integrated Security=True;Connect Timeout=30");
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            String query = "SELECT * from Login WHERE Username = '" + txtUsername.Text + "' and Password = '" + txtPassword.Text + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            if (table.Rows.Count == 1)
+            {
+                Main Main = new Main();
+                Main.ShowDialog();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid Credentials");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,8 @@ namespace Bug_Tracking_Application
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Login());
+
 
         }
     }
