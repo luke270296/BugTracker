af275751d7346e78e236cf8ec2844f4fbb654511
luke270296 <34241034+luke270296@users.noreply.github.com>
2018-01-06 04:15:46 +0000

    Added Functionality
    Made changes to database (SQL Server), added functionality to application.

--- Form1.cs
+++ Form1.cs
@@ -2,73 +2,93 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
-namespace Bug_Tracker
+namespace Bug_Tracking_Application
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        String[] bugid = new String[6]; // unique identifer for each bug being logged
-        int testerid; // unique identifer for the tester responsible for bug report
-        string testername; // name of tester inputting bug report
-        string application; // name of application where error exists, allows dev to narrow down problem
-        string classname; // class that bug exists within
-        int lineno; // the line number of the bug
-        string source; // link to the source code (bitbucket, pastebin)
-        string errordesc; // description of error, used by developer to recreate problem
-        string status; // has the error been resolved? has it been acknowledged by dev team?
-        DateTime date = DateTime.Now;
-
-        public Form1()
+        public Main()
         {
             InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Luke\Documents\BugData.mdf;Integrated Security=True;Connect Timeout=30");
+
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            String query = "INSERT INTO Bug (Tester_ID,Tester_Name,Application_Name,Class_Name,Line_No,Error_Description,Source_Code,Status) VALUES('" + txtID.Text + "','" + txtName.Text + "','" + txtApp.Text + "','" + txtClass.Text + "','" + txtLineNo.Text + "','" + txtDesc.Text + "','" + txtSource.Text + "','" + txtStatus.Text + "')";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.SelectCommand.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Data Inserted Successfully.");
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            String query = "SELECT * FROM Bug ";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtID.Clear();
+            txtApp.Clear();
+            txtDesc.Clear();
+            txtClass.Clear();
+            txtLineNo.Clear();
+            txtSource.Clear();
+            txtStatus.Clear();
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
             
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        private void btnUpdate_Click(object sender, EventArgs e)
         {
-            System.Windows.Forms.Application.Exit(); 
+            connection.Open();
+            String query = "UPDATE Bug SET Tester_Name = '" + txtName.Text + "',Application_Name = '" + txtApp.Text + "',Class_Name = '" + txtClass.Text + "', Line_No = '" + txtLineNo.Text + "',Error_Description = '" + txtDesc.Text + "',Source_Code = '" + txtSource.Text + "',Status = '" + txtStatus.Text + "'";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+            adapter.SelectCommand.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Data Updated Successfully");
         }
 
-        public void dataInsert()
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            // items will be added to database to log error
-            SqlConnection mySqlConnection;
-
-            mySqlConnection =
-            new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Luke\source\repos\Bug Tracker\BugTracker.accdb;Integrated Security=True;Connect Timeout=30");
-            String select = "SELECT BugID FROM Bug";
-            SqlCommand selectcmd = new SqlCommand(select, mySqlConnection);
-
-            mySqlConnection.Open();
-
-            SqlDataReader mySqlDataReader = selectcmd.ExecuteReader();
-            int i = 0;
-            while (mySqlDataReader.Read())
-            {
-                Console.WriteLine(mySqlDataReader["BugID"]);
-            }
-
-
-
+            txtID.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txtName.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            txtApp.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txtClass.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txtLineNo.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+            txtDesc.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            txtSource.Text = dataGridView1.SelectedRows[0].Cells[6].Value.ToString();
+            txtStatus.Text = dataGridView1.SelectedRows[0].Cells[7].Value.ToString();
         }
 
-        public void dataUpdate()
+        private void btnRemove_Click(object sender, EventArgs e)
         {
-            // make amendments to database table when saving changes to code
-            // use of button to edit table records? 'save' menu item saves local file for further development?
-            // change current status (as above)
-        }
-
-        private void btnAdd_Click(object sender, EventArgs e)
-        {
-            dataInsert();
+            connection.Open();
+            String query = "DELETE FROM Bug WHERE Tester_ID = '"+txtID+"";
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            adapter.SelectCommand.ExecuteNonQuery();
+            connection.Close();
+            MessageBox.Show("Record Successfully Deleted");
         }
     }
-
 }
